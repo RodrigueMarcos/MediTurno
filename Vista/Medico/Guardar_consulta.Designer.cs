5f9419f65632e7884f26901b6a211fd7d11af655
RodrigueMarcos <computer22marcos@gmail.com>
2024-01-24 11:47:56 -0300

    Version 1.1

---
+++ Vista/Medico/Guardar_consulta.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Vista.Medico
+{
+    partial class Guardar_consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L_nombreCompleto = new System.Windows.Forms.Label();
+            this.L_cobertura = new System.Windows.Forms.Label();
+            this.L_dni = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.L_Diagnostico = new System.Windows.Forms.Label();
+            this.L_Observaciones = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // L_nombreCompleto
+            // 
+            this.L_nombreCompleto.AutoSize = true;
+            this.L_nombreCompleto.Location = new System.Drawing.Point(146, 74);
+            this.L_nombreCompleto.Name = "L_nombreCompleto";
+            this.L_nombreCompleto.Size = new System.Drawing.Size(98, 13);
+            this.L_nombreCompleto.TabIndex = 0;
+            this.L_nombreCompleto.Text = "Apellido y Nombre :";
+            // 
+            // L_cobertura
+            // 
+            this.L_cobertura.AutoSize = true;
+            this.L_cobertura.Location = new System.Drawing.Point(149, 107);
+            this.L_cobertura.Name = "L_cobertura";
+            this.L_cobertura.Size = new System.Drawing.Size(59, 13);
+            this.L_cobertura.TabIndex = 1;
+            this.L_cobertura.Text = "Cobertura :";
+            // 
+            // L_dni
+            // 
+            this.L_dni.AutoSize = true;
+            this.L_dni.Location = new System.Drawing.Point(152, 142);
+            this.L_dni.Name = "L_dni";
+            this.L_dni.Size = new System.Drawing.Size(32, 13);
+            this.L_dni.TabIndex = 2;
+            this.L_dni.Text = "DNI :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // L_Diagnostico
+            // 
+            this.L_Diagnostico.AutoSize = true;
+            this.L_Diagnostico.Location = new System.Drawing.Point(227, 256);
+            this.L_Diagnostico.Name = "L_Diagnostico";
+            this.L_Diagnostico.Size = new System.Drawing.Size(63, 13);
+            this.L_Diagnostico.TabIndex = 4;
+            this.L_Diagnostico.Text = "Diagnostico";
+            // 
+            // L_Observaciones
+            // 
+            this.L_Observaciones.AutoSize = true;
+            this.L_Observaciones.Location = new System.Drawing.Point(130, 256);
+            this.L_Observaciones.Name = "L_Observaciones";
+            this.L_Observaciones.Size = new System.Drawing.Size(78, 13);
+            this.L_Observaciones.TabIndex = 5;
+            this.L_Observaciones.Text = "Observaciones";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(230, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(227, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(230, 152);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // Guardar_consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.L_Observaciones);
+            this.Controls.Add(this.L_Diagnostico);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.L_dni);
+            this.Controls.Add(this.L_cobertura);
+            this.Controls.Add(this.L_nombreCompleto);
+            this.Name = "Guardar_consulta";
+            this.Text = "Guardar_consulta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label L_nombreCompleto;
+        private System.Windows.Forms.Label L_cobertura;
+        private System.Windows.Forms.Label L_dni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label L_Diagnostico;
+        private System.Windows.Forms.Label L_Observaciones;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Vista/Secretario/Alta_paciente.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Vista.Secretario
+{
+    partial class Alta_paciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L_apellido = new System.Windows.Forms.Label();
+            this.L_nombre = new System.Windows.Forms.Label();
+            this.L_dni = new System.Windows.Forms.Label();
+            this.L_celular = new System.Windows.Forms.Label();
+            this.L_cobertura = new System.Windows.Forms.Label();
+            this.L_fecNacimiento = new System.Windows.Forms.Label();
+            this.L_correo = new System.Windows.Forms.Label();
+            this.B_Guardar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TB_Apellido = new System.Windows.Forms.TextBox();
+            this.TB_correo = new System.Windows.Forms.TextBox();
+            this.TB_nombre = new System.Windows.Forms.TextBox();
+            this.TB_dni = new System.Windows.Forms.TextBox();
+            this.TB_celular = new System.Windows.Forms.TextBox();
+            this.CB_cobertura = new System.Windows.Forms.ComboBox();
+            this.DTP_fecNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.L_contactos = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // L_apellido
+            // 
+            this.L_apellido.AutoSize = true;
+            this.L_apellido.ForeColor = System.Drawing.Color.White;
+            this.L_apellido.Location = new System.Drawing.Point(74, 55);
+            this.L_apellido.Name = "L_apellido";
+            this.L_apellido.Size = new System.Drawing.Size(44, 13);
+            this.L_apellido.TabIndex = 0;
+            this.L_apellido.Text = "Apellido";
+            // 
+            // L_nombre
+            // 
+            this.L_nombre.AutoSize = true;
+            this.L_nombre.ForeColor = System.Drawing.Color.White;
+            this.L_nombre.Location = new System.Drawing.Point(77, 106);
+            this.L_nombre.Name = "L_nombre";
+            this.L_nombre.Size = new System.Drawing.Size(44, 13);
+            this.L_nombre.TabIndex = 1;
+            this.L_nombre.Text = "Nombre";
+            // 
+            // L_dni
+            // 
+            this.L_dni.AutoSize = true;
+            this.L_dni.ForeColor = System.Drawing.Color.White;
+            this.L_dni.Location = new System.Drawing.Point(80, 150);
+            this.L_dni.Name = "L_dni";
+            this.L_dni.Size = new System.Drawing.Size(26, 13);
+            this.L_dni.TabIndex = 2;
+            this.L_dni.Text = "DNI";
+            // 
+            // L_celular
+            // 
+            this.L_celular.AutoSize = true;
+            this.L_celular.ForeColor = System.Drawing.Color.White;
+            this.L_celular.Location = new System.Drawing.Point(47, 27);
+            this.L_celular.Name = "L_celular";
+            this.L_celular.Size = new System.Drawing.Size(84, 13);
+            this.L_celular.TabIndex = 3;
+            this.L_celular.Text = "Teléfono Celular";
+            // 
+            // L_cobertura
+            // 
+            this.L_cobertura.AutoSize = true;
+            this.L_cobertura.ForeColor = System.Drawing.Color.White;
+            this.L_cobertura.Location = new System.Drawing.Point(328, 54);
+            this.L_cobertura.Name = "L_cobertura";
+            this.L_cobertura.Size = new System.Drawing.Size(53, 13);
+            this.L_cobertura.TabIndex = 4;
+            this.L_cobertura.Text = "Cobertura";
+            // 
+            // L_fecNacimiento
+            // 
+            this.L_fecNacimiento.AutoSize = true;
+            this.L_fecNacimiento.ForeColor = System.Drawing.Color.White;
+            this.L_fecNacimiento.Location = new System.Drawing.Point(331, 106);
+            this.L_fecNacimiento.Name = "L_fecNacimiento";
+            this.L_fecNacimiento.Size = new System.Drawing.Size(108, 13);
+            this.L_fecNacimiento.TabIndex = 5;
+            this.L_fecNacimiento.Text = "Fecha de Nacimiento";
+            // 
+            // L_correo
+            // 
+            this.L_correo.AutoSize = true;
+            this.L_correo.ForeColor = System.Drawing.Color.White;
+            this.L_correo.Location = new System.Drawing.Point(298, 26);
+            this.L_correo.Name = "L_correo";
+            this.L_correo.Size = new System.Drawing.Size(94, 13);
+            this.L_correo.TabIndex = 6;
+            this.L_correo.Text = "Correo Electronico";
+            // 
+            // B_Guardar
+            // 
+            this.B_Guardar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.B_Guardar.ForeColor = System.Drawing.Color.White;
+            this.B_Guardar.Location = new System.Drawing.Point(413, 371);
+            this.B_Guardar.Name = "B_Guardar";
+            this.B_Guardar.Size = new System.Drawing.Size(75, 23);
+            this.B_Guardar.TabIndex = 7;
+            this.B_Guardar.Text = "Guardar";
+            this.B_Guardar.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(331, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // TB_Apellido
+            // 
+            this.TB_Apellido.Location = new System.Drawing.Point(80, 82);
+            this.TB_Apellido.Name = "TB_Apellido";
+            this.TB_Apellido.Size = new System.Drawing.Size(100, 20);
+            this.TB_Apellido.TabIndex = 9;
+            // 
+            // TB_correo
+            // 
+            this.TB_correo.Location = new System.Drawing.Point(314, 47);
+            this.TB_correo.Name = "TB_correo";
+            this.TB_correo.Size = new System.Drawing.Size(100, 20);
+            this.TB_correo.TabIndex = 10;
+            // 
+            // TB_nombre
+            // 
+            this.TB_nombre.Location = new System.Drawing.Point(77, 127);
+            this.TB_nombre.Name = "TB_nombre";
+            this.TB_nombre.Size = new System.Drawing.Size(100, 20);
+            this.TB_nombre.TabIndex = 11;
+            // 
+            // TB_dni
+            // 
+            this.TB_dni.Location = new System.Drawing.Point(80, 172);
+            this.TB_dni.Name = "TB_dni";
+            this.TB_dni.Size = new System.Drawing.Size(100, 20);
+            this.TB_dni.TabIndex = 12;
+            // 
+            // TB_celular
+            // 
+            this.TB_celular.Location = new System.Drawing.Point(44, 47);
+            this.TB_celular.Name = "TB_celular";
+            this.TB_celular.Size = new System.Drawing.Size(100, 20);
+            this.TB_celular.TabIndex = 13;
+            // 
+            // CB_cobertura
+            // 
+            this.CB_cobertura.FormattingEnabled = true;
+            this.CB_cobertura.Location = new System.Drawing.Point(337, 82);
+            this.CB_cobertura.Name = "CB_cobertura";
+            this.CB_cobertura.Size = new System.Drawing.Size(121, 21);
+            this.CB_cobertura.TabIndex = 15;
+            // 
+            // DTP_fecNacimiento
+            // 
+            this.DTP_fecNacimiento.Location = new System.Drawing.Point(337, 142);
+            this.DTP_fecNacimiento.Name = "DTP_fecNacimiento";
+            this.DTP_fecNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.DTP_fecNacimiento.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.L_celular);
+            this.panel1.Controls.Add(this.L_correo);
+            this.panel1.Controls.Add(this.TB_correo);
+            this.panel1.Controls.Add(this.TB_celular);
+            this.panel1.Location = new System.Drawing.Point(86, 241);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(486, 100);
+            this.panel1.TabIndex = 17;
+            // 
+            // L_contactos
+            // 
+            this.L_contactos.AutoSize = true;
+            this.L_contactos.ForeColor = System.Drawing.Color.White;
+            this.L_contactos.Location = new System.Drawing.Point(105, 225);
+            this.L_contactos.Name = "L_contactos";
+            this.L_contactos.Size = new System.Drawing.Size(50, 13);
+            this.L_contactos.TabIndex = 14;
+            this.L_contactos.Text = "Contacto";
+            // 
+            // Alta_paciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.L_contactos);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DTP_fecNacimiento);
+            this.Controls.Add(this.CB_cobertura);
+            this.Controls.Add(this.TB_dni);
+            this.Controls.Add(this.TB_nombre);
+            this.Controls.Add(this.TB_Apellido);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.B_Guardar);
+            this.Controls.Add(this.L_fecNacimiento);
+            this.Controls.Add(this.L_cobertura);
+            this.Controls.Add(this.L_dni);
+            this.Controls.Add(this.L_nombre);
+            this.Controls.Add(this.L_apellido);
+            this.Name = "Alta_paciente";
+            this.Text = "Agregar paciente";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label L_apellido;
+        private System.Windows.Forms.Label L_nombre;
+        private System.Windows.Forms.Label L_dni;
+        private System.Windows.Forms.Label L_celular;
+        private System.Windows.Forms.Label L_cobertura;
+        private System.Windows.Forms.Label L_fecNacimiento;
+        private System.Windows.Forms.Label L_correo;
+        private System.Windows.Forms.Button B_Guardar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox TB_Apellido;
+        private System.Windows.Forms.TextBox TB_correo;
+        private System.Windows.Forms.TextBox TB_nombre;
+        private System.Windows.Forms.TextBox TB_dni;
+        private System.Windows.Forms.TextBox TB_celular;
+        private System.Windows.Forms.ComboBox CB_cobertura;
+        private System.Windows.Forms.DateTimePicker DTP_fecNacimiento;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label L_contactos;
+    }
+}
